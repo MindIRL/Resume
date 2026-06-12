1095fab9a5399b5176d37cfc696140596a615e0b
MindIRL <mymemildz18@gmail.com>
2026-06-12 12:48:55 +0700

    Add UptimeRobot fot runtime server every 5m

--- resume-back/Server.js
+++ resume-back/Server.js
@@ -13,15 +13,34 @@ const app = express()
 
 mongoose.set('strictQuery', false)
 
-mongoose.connect(process.env.DATABASE)
-.then(()=>console.log("เชื่อมต่อฐานข้อมูลสำเร็จ"))
-.catch((err)=>console.log("ไม่สามารถเชื่อมต่อฐานข้อมูลได้",err))
+// mongoose.connect(process.env.DATABASE)
+// .then(()=>console.log("เชื่อมต่อฐานข้อมูลสำเร็จ"))
+// .catch((err)=>console.log("ไม่สามารถเชื่อมต่อฐานข้อมูลได้",err))
+
+
+const MongoDB = async() =>{
+    try{
+        await mongoose.connect(process.env.DATABASE)
+        console.log("เชื่อมต่อฐานข้อมูลสำเร็จ")
+    }catch (err){
+        console.log("ไม่สามารถเชื่อมต่อฐานข้อมูลได้" , err)
+    }
+} 
+
+MongoDB()
 
 
 
 app.use(express.json())
 app.use(cors())
 app.use(morgan("dev"))
+
+
+
+//ใช้ UptimeRobot ยิง req เพื่อไม่ให้ server หลับ runtime ตลอด 
+app.get("/health" , (req , res)=>{
+    res.json({ok:true})
+})
 
 
 
